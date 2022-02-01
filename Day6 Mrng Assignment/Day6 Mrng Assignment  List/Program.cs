c44b2635d780e0eeabd476d8f842d782f3b147db
uddandijoshna <joshna.u7@gmail.com>
2022-02-01 09:25:39 +0530

    second submission

---
+++ Day6 Mrng Assignment/Day6 Mrng Assignment  List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6_Mrng_Assignment__List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() { 1, 2, 3, 4 };
+            int sum = intList.Aggregate((x, y) => x + y);
+        }
+    }
+}
---
+++ Day6 Mrng Assignment/Day6 Mrng Assignment ArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+namespace Day6_Mrng_Assignment_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            ArrayList data = new ArrayList();
+
+            data.Add(10);
+            data.Add(20);
+            data.Add(30);
+            data.Add(40);
+            data.Add(50);
+
+            {
+                sum = sum + Convert.ToInt32(data.Count);
+            }
+            Console.WriteLine($"Sum of data is {sum}");
+            Console.ReadLine();
+            }
+
+
+        }
+    }
+
